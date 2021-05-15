0c21289816335713bd4c232cb36d4ae5c5fccc90
IanMalabanan <56718018+IanMalabanan@users.noreply.github.com>
2021-05-16 06:06:51 +0800

    Commit

--- HRinformationSystem/EnablePaySlipViewing.xaml.cs
+++ HRinformationSystem/EnablePaySlipViewing.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,74 @@ namespace HRinformationSystem
     /// </summary>
     public partial class EnablePaySlipViewing : Window
     {
+        public static string _monthYear, gross, net, deduct, basic;
+
         public EnablePaySlipViewing()
         {
             InitializeComponent();
+
+            LoadFields();
+        }
+
+        private void LoadFields()
+        {
+            cboCutOff.Items.Clear();
+            cboCutOff.Items.Add("1st Quincena");
+            cboCutOff.Items.Add("2nd Quincena");
+
+
+            for (int i = 0; i < 12; i++)
+            {
+                cboMonth.Items.Insert(i, CultureInfo.CurrentUICulture.DateTimeFormat.MonthNames[i]);
+            }
+
+            //cboMonth.Items.Insert(12, "13th Month");
+
+            //cboMonth.SelectedIndex = DateTime.Today.Month - 1;
+
+            for (int i = DateTime.Now.Year; i >= DateTime.Now.Year - 4; i--)
+            {
+                cboYear.Items.Add(i);
+            }
+
+            cboYear.SelectedItem = Convert.ToString(DateTime.Today.Year);
+
+        }
+
+        private String ConcatenanteMonthYear()
+        {
+            string sYear;
+            string sMonth;
+            Int16 iMonth;
+
+            sYear = cboYear.Text;
+            iMonth = Convert.ToInt16(cboMonth.SelectedIndex + 1);
+
+            switch (iMonth)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    {
+                        sMonth = "0" + iMonth.ToString();
+                        break;
+                    }
+                default:
+                    {
+                        sMonth = iMonth.ToString();
+                        break;
+                    }
+            }
+
+            _monthYear = sMonth + sYear;
+
+            return _monthYear;
         }
     }
 }
